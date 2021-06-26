f463b1ebb6a83eeeb14469a42bd52f8c58ff5a4b
Paula Leal <paula61395@gmail.com>
2021-06-25 21:07:00 -0400

    started styling home page

--- bites-restaurant/src/App.vue
+++ bites-restaurant/src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/menu">Menu</router-link> |
-    <router-link to="/cart">Cart</router-link> |
-  </div>
   <router-view />
 </template>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- bites-restaurant/src/views/Home.vue
+++ bites-restaurant/src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
-  <div id="wrapper">
+  <header>
     <h1>Bites</h1>
+  </header>
+  <div id="welcomeBox">
+    <div id="welcomeContent">
+      <h2>Bite Sized Goodness</h2>
+      <button>Order</button>
+    </div>
   </div>
 </template>
 
@@ -11,13 +17,54 @@ export default {
   components: {},
 };
 </script>
-<style scoped lang="scss">
+<style lang="scss">
 body {
-  background: url("../../public/images/burger1.jpg") no-repeat center center
-    fixed;
+  background: url("../assets/images/burger1.jpg") no-repeat center center fixed;
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
   background-size: cover;
+}
+
+header {
+  opacity: 0.88;
+  background-color: #343a40;
+  color: #fff;
+
+  h1 {
+    font-size: 70px;
+    letter-spacing: 1rem;
+  }
+}
+
+#welcomeBox {
+  opacity: 0.88;
+  background-color: #343a40;
+  color: #fff;
+  width: 400px;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+
+  div {
+    border: 1px orange solid;
+
+    h2 {
+      font-size: 40px;
+      margin: 1rem;
+    }
+
+    button {
+      padding: 10px;
+      border-radius: 25px;
+      border: none;
+      font-size: 20px;
+      background-color: red;
+      color: #fff;
+      font-weight: bold;
+    }
+  }
 }
 </style>
