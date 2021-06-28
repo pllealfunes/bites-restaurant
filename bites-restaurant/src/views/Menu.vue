65ecbea72a2ef47cb102cde535452e7d3a68c133
Paula Leal <paula61395@gmail.com>
2021-06-27 21:29:01 -0400

    started work on reading all food entries in data folder

--- bites-restaurant/src/views/Menu.vue
+++ bites-restaurant/src/views/Menu.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="wrapper">
     <h1>Menu</h1>
+    <ul>
+      <li v-for="food in foods" :key="food">{{ food }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: "Menu",
+  data() {
+    return { food: null };
+  },
   props: {},
+  methods: {
+    async readMenu() {
+      const res = await fetch("http://localhost:3000/food");
+      let data = await res.json();
+      return data;
+      //console.log(foods);
+    },
+  },
+  async created() {
+    const data = await this.readMenu();
+    this.food = data;
+    console.log(data);
+  },
 };
 </script>
 
