952f426552118751f6b971281a553bbfb172edef
Paula Leal <paula61395@gmail.com>
2021-09-25 00:13:56 -0400

    redid dist

--- bites-restaurant/server.js
+++ bites-restaurant/server.js
@@ -5,6 +5,6 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
-server.listen(process.env.PORT || 4000, () => {
+server.listen(process.env.PORT || 3000, () => {
     console.log('JSON Server is running')
 })
