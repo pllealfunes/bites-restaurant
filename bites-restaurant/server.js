db1dca5bea2c658f4153954e83e3c6fd20838f43
Paula Leal <paula61395@gmail.com>
2021-09-25 21:22:05 -0400

    rebuilt dist

--- bites-restaurant/server.js
+++ bites-restaurant/server.js
@@ -2,7 +2,9 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const serveStatic = require('serve-static')
 
+server.use(serveStatic(__dirname + "/dist"))
 server.use(middlewares)
 server.use(router)
 server.listen(process.env.PORT || 3000, () => {
