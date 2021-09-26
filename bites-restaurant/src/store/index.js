5eadb423dc3cdc0b3458d44fa364c73cca1d0adc
Paula Leal <paula61395@gmail.com>
2021-09-25 22:05:18 -0400

    edited fetch in store/ index.js

--- bites-restaurant/src/store/index.js
+++ bites-restaurant/src/store/index.js
@@ -35,7 +35,11 @@ const store = createStore({
   // by committing mutations
   actions: {
     getMenu({ commit }) {
-      fetch("http://localhost:3000/menu")
+      fetch("https://bites-restaurant.herokuapp.com/menu", {
+        method: 'GET',
+        mode: 'cors',
+        headers: { 'Content-Type': 'text' }
+      })
         .then((res) => res.json())
         .then((data) => (commit("SET_MENU", data)))
         .catch((err) => console.log(err.message));
