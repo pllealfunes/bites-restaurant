be0f1bfa8eb088f332ad0de7f95cc7db7b0886ac
Paula Leal <paula61395@gmail.com>
2021-09-25 23:02:03 -0400

    changed fetch call

--- bites-restaurant/src/store/index.js
+++ bites-restaurant/src/store/index.js
@@ -35,7 +35,7 @@ const store = createStore({
   // by committing mutations
   actions: {
     getMenu({ commit }) {
-      fetch("https://bites-restaurant.herokuapp.com/menu", {
+      fetch("http://localhost:3000/menu", {
         method: 'GET',
         mode: 'cors',
         headers: { 'Content-Type': 'application/json' }
