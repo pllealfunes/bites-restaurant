8e88c6cfce22083b3461f3beaccae94b7bdf3d54
Paula Leal <paula61395@gmail.com>
2021-09-27 13:07:33 -0400

    removed served static in server.js

--- bites-restaurant/server.js
+++ bites-restaurant/server.js
@@ -1,11 +1,8 @@
 const jsonServer = require('json-server')
-const path = require('path')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-//const serveStatic = require('serve-static')
 
-//server.use(serveStatic(__dirname + "/dist"))
 server.use(middlewares)
 server.use(router)
 server.listen(process.env.PORT || 3000, () => {
