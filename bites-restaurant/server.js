41c3a109d6cd1dc6e15807fe20647ace6ce63679
Paula Leal <paula61395@gmail.com>
2021-09-27 13:53:36 -0400

    edited port in server.js

--- bites-restaurant/server.js
+++ bites-restaurant/server.js
@@ -3,8 +3,9 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
+
 server.use(middlewares)
 server.use(router)
-server.listen(process.env.PORT || 3000, () => {
+server.listen(process.env.PORT || 4000, () => {
     console.log('JSON Server is running')
 })
