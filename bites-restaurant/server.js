f7278798e229610407edf0dd051251bf03522525
Paula Leal <paula61395@gmail.com>
2022-12-14 15:16:37 -0500

    added lines to help with cors

--- bites-restaurant/server.js
+++ bites-restaurant/server.js
@@ -6,6 +6,10 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
+server.use((req, res, next) => {
+    res.header('ACCESS-CONTROL-ALLOW-ORIGIN', 'https://bites-restaurant.herokuapp.com/menu'),
+        res.header('ACCESS-CONTROL-ALLOW-Headers', '*')
+})
 server.listen(process.env.PORT || 4000, () => {
     console.log('JSON Server is running')
 })
