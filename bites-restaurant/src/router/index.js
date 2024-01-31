f6665b91489dbb194860f540be5033a15c021b04
pllealfunes <51715816+pllealfunes@users.noreply.github.com>
2024-01-31 13:27:45 -0500

    revreted back to old createWebHistory

--- bites-restaurant/src/router/index.js
+++ bites-restaurant/src/router/index.js
@@ -39,7 +39,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory("https://bites-restaurant.onrender.com"),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
